888f04be19f6844daa040b77593be9001f99b6d9
gigi <150442603+iigigii@users.noreply.github.com>
2026-03-21 12:20:16 +0100

    Add files via upload

---
+++ TASK 4/Server/iotgwda.py
@@ -0,0 +1,116 @@
+#importazione librerie
+import socket #per la gestione delle socket
+import time #per la gestione del tempo
+import json #per gestire i file json
+import cripto #per la gestione della crittografia
+import threading #per la gestione dei thread
+
+#leggo i parametri presenti nel file: parametri.json
+with open("configurazione/parametri.json", "r") as file:
+    parametri = json.load(file)
+
+TEMPO_RILEVAZIONE = parametri["TEMPO_RILEVAZIONE"]
+TEMPO_INVIO = parametri["TEMPO_INVIO"] 
+NUMERO_DECIMALI = parametri["NUMERO_DECIMALI"]
+IP_SERVER = parametri["IP_SERVER"]
+PORTA_SERVER = parametri["PORTA_SERVER"]
+
+#funzione per gestire la connessione con il client 
+def gestione_client(client, indirizzo):
+    #invio al DC il tempo di rilevazione
+    client.send(str(parametri["TEMPO_RILEVAZIONE"]).encode())   
+
+    #dichiarazione e inizializzazione variabili
+    temperature = []
+    umidita = []
+    invio_numero = 0
+    inizio = time.time() 
+
+    try:
+        #finchè la connessione è attiva, ricevo dati
+        while True:
+            #ricevo i dati dal client
+            dato = client.recv(1024).decode()
+            #se la connessione viene chiusa, esco dal ciclo
+            if not dato:
+                break
+
+            #decriptazione del dato ricevuto
+            dato_json = cripto.decriptazione(dato)
+            dato = json.loads(dato_json)
+
+            #aggiungo i dati ricevuti agli array
+            temperature.append(dato["osservazione"]["temperatura"])
+            umidita.append(dato["osservazione"]["umidita"])
+
+            #controllo il tempo di invio
+            if time.time() - inizio >= TEMPO_INVIO:
+                #incremento il numeo di invii
+                invio_numero = invio_numero + 1 
+
+                #calcolo la media delle temperature e delle umidità
+                media_t = round(sum(temperature) / len(temperature), NUMERO_DECIMALI)
+                media_u = round(sum(umidita) / len(umidita), NUMERO_DECIMALI)
+
+                #calcolo la data e ora della rilevazione
+                rilevazione_data_ora = time.time()
+
+                #creo il dizionario da inviare alla platform
+                dato_iot = {
+                    "invio_numero": invio_numero,
+                    "identita_giot": parametri["ID_GIOT"],
+                    "cabina": dato["cabina"],
+                    "ponte": dato["ponte"],
+                    "data_ora": rilevazione_data_ora,
+                    "media_temperatura": media_t,
+                    "media_umidita": media_u,
+                    "dc": dato["identita"],
+                }
+
+                #salvataggio nel file iotdata.dvt
+                with open("iotp/db.json", "a") as file:
+                    file.write(json.dumps(dato_iot) + "\n")
+
+                #crittografia del dato
+                dato_json = json.dumps(dato_iot)
+                dato_cripto = cripto.criptazione(dato_json)
+
+                #resetto l'array delle temperature, delle umidità e la data di inizio
+                temperature = []
+                umidita = []
+                inizio = time.time()
+
+    except Exception:
+        print(f"Errore con DC")
+        print(f"(Connessione chiusa con {indirizzo}. Numero invii: {invio_numero}")
+
+    finally:
+
+        client.close()
+
+#server multithread
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creazione di una socket tcp/ipv4 
+server_socket.bind((IP_SERVER, PORTA_SERVER)) #"collego" la socket all'ip e porta specificti
+server_socket.listen() #ascolto più connessioni 
+
+#mostro un messaggio in output per indicare che il DA è in ascolto
+print("DA avviato. In attesa del DC..")
+
+try:
+    while True:
+        #dichiarazione e inizializzazione variabili
+        client, indirizzo = server_socket.accept() #accetto la connessione in entrata
+
+        #mostro un messaggio in output per indicare che il DC e il DA sono connessi
+        print("Connessione con DC effettuata da:", indirizzo)
+
+        #creazione del thread
+        thread = threading.Thread(target=gestione_client, args=(client, indirizzo))
+        thread.daemon = True
+        thread.start()
+
+except KeyboardInterrupt:
+    print("DA chiuso")
+
+finally:
+    server_socket.close()
